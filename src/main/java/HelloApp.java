638d9eeb60f8fcfc47a13acfbec244993ce9f694
IvoStam <ivelinstamatov@gmail.com>
2017-12-06 14:39:16 +0100

    kleine wijziging. develop new Class new brenge New helloApp end shalve changes some new methods in Hello

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,6 +1,17 @@
 public class HelloApp {
     public static void main(String[] args) {
         Hello hello1 = new Hello("World");
+        Hello hello2 = new Hello();
+        hello2.setToWho("Gert");
+        Hello hello3 = new Hello();
+
+        hello1.sayHello();
+        hello2.sayHello();
+        hello3.sayHello();
+
+
+
+
         
     }
 }
