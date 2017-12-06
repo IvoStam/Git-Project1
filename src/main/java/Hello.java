6b56f260e3c0b7ef9959319f9d89166845a667be
IvoStam <ivelinstamatov@gmail.com>
2017-12-06 14:13:46 +0100

    kleine wijziging. develop new Class new brench New helloApp

---
+++ src/main/java/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+    private final String GREETING = "Hello";
+    private String word ;
+
+    public Hello(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
