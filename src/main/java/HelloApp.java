0912795187dd63f99378d9fa6450f9595e75c717
IvoStam <ivelinstamatov@gmail.com>
2019-02-02 23:24:46 +0100

    Added String Dinko.

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -4,14 +4,10 @@ public class HelloApp {
         Hello hello2 = new Hello();
         hello2.setToWho("Ivo");
         Hello hello3 = new Hello();
-
+        hello3.setToWho("Dinko");
         hello1.sayHello();
         hello2.sayHello();
         hello3.sayHello();
-
-
-
-
         
     }
 }
