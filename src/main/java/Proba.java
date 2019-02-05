cebb0c4861035ca36dfb010dfe72fc6b4fda0a97
DINC <dkumanov1976@gmail.com>
2019-02-05 12:05:43 +0200

    new class Proba

--- src/main/java/Proba.java
+++ src/main/java/Proba.java
@@ -1,2 +1,9 @@
+import java.util.Random;
+
 public class Proba {
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextInt(20));
+    }
 }
+
