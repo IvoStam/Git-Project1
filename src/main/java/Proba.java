bfc08719d8b2766e19667adf74b8557e8442c0c7
DINC <dkumanov1976@gmail.com>
2019-02-07 22:03:50 +0200

    new class Proba

--- src/main/java/Proba.java
+++ src/main/java/Proba.java
@@ -1,9 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Random;
 
 public class Proba {
-    public static void main(String[] args) {
-        Random r = new Random();
-        System.out.println(r.nextInt(20));
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader
+                (new InputStreamReader(System.in));
+
+        String [] first = reader.readLine().split(" ");
+        String [] second = reader.readLine().split(" ");
+        for (String sec:second) {
+            for (String fir:first) {
+
+                if (sec.equals(fir))
+                    System.out.print(sec + " ");
+                }
+
+            }
+            
+        }
+
     }
-}
+
 
